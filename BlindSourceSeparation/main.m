93049fa3d89ced40a0fc24f1f8be6142dd057ea4
nayansinghal <singhalnayan91@gmail.com>
2017-02-21 10:13:13 -0600

    for loop for learning rate, iterations, and averaging

--- BlindSourceSeparation/main.m
+++ BlindSourceSeparation/main.m
@@ -1,9 +1,30 @@
-load('icaTest.mat');
-NumOfSignals = 3;
-X = A*U;
+clear all;
+clc;
+close all;
 
-learningRate = 0.01;
-numIters = 100;
+load('sounds.mat');
+NumOfSignals =3;
+NumOfSamples = 3;
+index = [1,3,4];
+U = sounds(index,:);
+%X = A*U;
+[mixedSignals, A] = mixSignals(sounds, NumOfSignals, NumOfSamples, index);
+matrixError = [];
 
-Y = ICA(learningRate, numIters, X, NumOfSignals);
-plotResults(X, Y, U);
+% Varying learning rate
+for learningRate = 0.01: 0.01:0.01
+    % Varying number of iterations
+    for numIters = 1000:1000:100000
+        
+        error = 0;
+        % Varying count number
+        for count = 1:1:10
+
+            [Y,W] = ICA(learningRate, numIters, mixedSignals, NumOfSamples);
+            error = error + (norm(pinv(A)) - norm(W));
+        end;
+        matrixError = [matrixError; error/10];
+    end;
+end;
+plot([1000:1000:100000], matrixError)
+%plotResults(U, mixedSignals, Y);
