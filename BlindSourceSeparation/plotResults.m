ab68c5f3190cb56afa3a72a2c0dab2ce4da85f58
nayansinghal <singhalnayan91@gmail.com>
2017-02-21 10:11:03 -0600

    Add saveAudio function

--- BlindSourceSeparation/plotResults.m
+++ BlindSourceSeparation/plotResults.m
@@ -1,9 +1,12 @@
 function [  ] = plotResults( U, X, Y )
 %UNTITLED8 Summary of this function goes here
 %   Detailed explanation goes here
-plotWaves(U, 'original signals');
-plotWaves(X, 'Mixed signals');
-plotWaves(Y, 'Extracted signals');
+plotWaves(U, 'output/original signals');
+plotWaves(X, 'output/Mixed signals');
+plotWaves(Y, 'output/Reconstructed signals');
 
+saveAudio(U, 'output/original signals');
+saveAudio(X, 'output/Mixed signals');
+saveAudio(Y, 'output/Reconstructed signals');
 end
 
