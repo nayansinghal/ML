b62ca7af457103c29a0aea2cdead216b4c9d26eb
nayansinghal <singhalnayan91@gmail.com>
2017-02-21 10:11:48 -0600

    Rescale waves [-1,1]

--- BlindSourceSeparation/plotWaves.m
+++ BlindSourceSeparation/plotWaves.m
@@ -1,11 +1,14 @@
 function [ ] = plotWaves( A, title )
 %UNTITLED3 Summary of this function goes here
 %   Detailed explanation goes here
+    A = rescaleMatrix(A, -1,1);
     fig = figure('name', title);
     color = ['r','m','b','g','k'];
+    
+    %plot all the signals
     for i = 1:1: size(A,1)
         subplot(size(A,1),1,i);
-        plot(A(i,:),color(i))
+        plot(A(i,:))
         xlabel(sprintf('Signal %d',i));
         ylabel('Amplitude');
     end;
