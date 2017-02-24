8a2a8083c5ee6b10dac47a657ec5ec61936c589a
nayansinghal <singhalnayan91@gmail.com>
2017-02-24 14:06:46 -0600

    Add title argument

--- BlindSourceSeparation/plotResults.m
+++ BlindSourceSeparation/plotResults.m
@@ -1,12 +1,14 @@
-function [  ] = plotResults( U, X, Y )
+function [  ] = plotResults( U, X, Y , i)
 %UNTITLED8 Summary of this function goes here
 %   Detailed explanation goes here
-plotWaves(U, 'output/original signals');
-plotWaves(X, 'output/Mixed signals');
-plotWaves(Y, 'output/Reconstructed signals');
+plotWaves(U, strcat('output/combination/original signals', num2str(i)));
+plotWaves(X, strcat('output/combination/Mixed signals', num2str(i)));
+plotWaves(Y, strcat('output/combination/Reconstructed signals', num2str(i)));
+close all;
 
 saveAudio(U, 'output/original signals');
 saveAudio(X, 'output/Mixed signals');
 saveAudio(Y, 'output/Reconstructed signals');
+
 end
 
