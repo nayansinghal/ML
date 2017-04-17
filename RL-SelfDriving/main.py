2eaf3aa0f6598b6f61dc17e5570ca54c69afaa98
nayansinghal <singhalnayan91@gmail.com>
2017-04-17 00:36:41 -0500

    pass static obstacle paramter

--- RL-SelfDriving/main.py
+++ RL-SelfDriving/main.py
@@ -6,13 +6,17 @@ if __name__ == '__main__':
 	width = 10
 	height = 2
 
-	grid = Gridworld(width, height)
+	grid = Gridworld(width, height, "obstacle")
 	grid.makeGrid()
 	agent = QLearningAgent(grid)
 
-	NumOfEpisode = 20
+	NumOfEpisode = 200
 
 	display = graphicsGridworldDisplay.GraphicsGridworldDisplay(grid, 120, 1)
 
+	for episode in range(0, NumOfEpisode):
+		grid.runEpisode(agent, episode, display)
+
+	agent.setEpsilon(0.0)
 	for episode in range(0, NumOfEpisode):
 		grid.runEpisode(agent, episode, display)
