caaa3ae4863fb2b6e9b3239758342ae8cac89bc1
nayansinghal <singhalnayan91@gmail.com>
2017-04-20 08:34:21 -0500

    add stop, traffic light basic version

--- RL-SelfDriving/gridworld.py
+++ RL-SelfDriving/gridworld.py
@@ -18,6 +18,8 @@ class Gridworld():
     self.startState = (0,0)
     self.qtype = qtype
     self.dynamicCars = []
+    self.trafficLights = []
+    self.trafficColors = {'R':0, 'Y':1, 'G':2, '2':'G', '1':'Y', '0':'R'}
 
   def getStartState(self):
     return self.startState
@@ -44,6 +46,33 @@ class Gridworld():
         self.dynamicCars.append([5, 1])
         for cars in self.dynamicCars:
           self.data[cars[0]][cars[1]] = 'D'
+
+      if self.qtype == "trafficLight":
+        self.trafficLights.append([5,0])
+        for trafficLight in self.trafficLights:
+          self.data[trafficLight[0]][trafficLight[1]] = 'R'
+
+  def getTrafficEnv(self, state):
+    trafLightSts = ''
+
+    x,y = state
+    if (x == self.width - 1):
+      trafLightSts = ''
+    elif (self.data[x+1][1] == 'R' or self.data[x + 1][0] == 'R'):
+      trafLightSts = 'R'
+    elif (self.data[x+1][1] == 'Y' or self.data[x+1][0] == 'Y'):
+      trafLightSts = 'Y'
+    elif (self.data[x + 1][1] == 'G' or self.data[x + 1][0] == 'G'):
+      trafLightSts = 'G'
+    else:
+      trafLightSts = ''
+
+    return trafLightSts
+
+  def updateTrafficEnv(self):
+    for trafficLight in self.trafficLights:
+      x,y = trafficLight[0], trafficLight[1]
+      self.data[x][y] = self.trafficColors[str((int(self.trafficColors[self.data[x][y]]) + 1)%3)]
 
   def getObstacleEnv(self, state):
       north = ' '
@@ -81,14 +110,14 @@ class Gridworld():
 
     x, y = state
     if (y == self.height - 1):
-      backward = 'w'
-      same = 'w'
+      backward = 'W'
+      same = 'W'
     elif (x == 0):
-      backward = 'w'
-      same = self.data[x][y]
+      backward = 'W'
+      same = self.data[x][1] if self.data[x][1] is '' or self.data[x][1] is 'D' else ''
     else:
-      backward = self.data[x-1][1]
-      same = self.data[x][1]
+      backward = self.data[x-1][1] if self.data[x-1][1] is '' or self.data[x-1][1] is 'D' else ''
+      same = self.data[x][1] if self.data[x][1] is '' or self.data[x][1] is 'D' else ''
 
     dynamice_env = (backward, same)
     return dynamice_env
@@ -102,6 +131,8 @@ class Gridworld():
       return self.getObstacleEnv(state)
     elif self.qtype == "dynamic_obstacle":
       return self.getDynObsEnv(state)
+    elif self.qtype == "trafficLight":
+      return self.getTrafficEnv(state)
 
   def updateEnvironment(self):
 
@@ -120,7 +151,7 @@ class Gridworld():
 
     if x==(self.width-1) and y ==(0):
       return ('exit',)
-    return ('north','east','south')
+    return ('north','east','south', 'stop')
     
   def getStates(self):
     """
@@ -158,6 +189,18 @@ class Gridworld():
         if x==(self.width-1) and y ==(0):
           return 10
 
+    elif self.qtype == "trafficLight":
+      if nextState != self.terminalState:
+        x, y = nextState
+        for trafficLight in self.trafficLights:
+          trafficX, trafficY = trafficLight[0], trafficLight[1]
+          if x == trafficX and y == trafficY and (self.data[trafficX][trafficY] == 'R' or self.data[trafficX][trafficY] == 'Y'):
+            return -5.0
+          else:
+            return 10.0
+        if x == (self.width - 1) and y == (0):
+          return 10
+
     if action=='east':
       return 0.5
     else:
@@ -177,6 +220,8 @@ class Gridworld():
       action='east'
     elif(y1==y2 and x2==x1-1):
       action='west'
+    elif(y1==y2 and x2==x1):
+      action = 'stop'
     return action
 
   def doAction(self, action):
@@ -236,7 +281,9 @@ class Gridworld():
       # UPDATE LEARNER
       agent.update(state, action2, nextState, reward)
       self.updateEnvironment()
+      self.updateTrafficEnv()
       agent.updateEpsilon()
+      print 'color:', self.data[5][0]
 
   def getTransitionStatesAndProbs(self, state, action):
     if action not in self.getPossibleActions(state):
@@ -277,6 +324,9 @@ class Gridworld():
       massLeft = self.noise
       successors.append((northState, massLeft / 2.0))
       successors.append((southState, massLeft / 2.0))
+
+    if action == 'stop':
+      successors.append(((x,y), 1.0))
 
     successors = self.__aggregate(successors)
 
