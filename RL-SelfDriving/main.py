dc71ac20fca837c49616c9483cd53afdc2f89523
nayansinghal <singhalnayan91@gmail.com>
2017-04-19 22:49:57 -0500

    checked all q values for dynamic obstacles

--- RL-SelfDriving/main.py
+++ RL-SelfDriving/main.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
 	width = 10
 	height = 2
 
-	grid = Gridworld(width, height, "obstacle")
+	grid = Gridworld(width, height, "dynamic_obstacle")
 	grid.makeGrid()
 	agent = QLearningAgent(grid)
 
@@ -17,6 +17,13 @@ if __name__ == '__main__':
 	for episode in range(0, NumOfEpisode):
 		grid.runEpisode(agent, episode, display)
 
-	agent.setEpsilon(0.0)
-	for episode in range(0, NumOfEpisode):
-		grid.runEpisode(agent, episode, display)
+	dynamic_state = ['W', 'D','']
+	actions = ['north','east','south']
+	for action in actions:
+		for i in range(0,3):
+			for j in range(0,3):
+				state = dynamic_state[i], dynamic_state[j]
+				print((dynamic_state[i], dynamic_state[j], action) ,agent.getQvalueUsingDynObstacle(state, action))
+	#agent.setEpsilon(0.0)
+	#for episode in range(0, NumOfEpisode):
+	#	grid.runEpisode(agent, episode, display)
